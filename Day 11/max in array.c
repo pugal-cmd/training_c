bfa9ab500ced008ac61c67ec64bd67535dc9c2cb
PUGAL A <gokultimer@gmail.com>
2025-06-21 15:27:15 +0530

    Create max in array.c

---
+++ Day 11/max in array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, max=0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if(arr[i]>max)
+        max=arr[i];
+    }
+
+    printf("The maximum element in the array is: %d\n", max);
+
+    return 0;
+}
